714c7d77ffb7df2b75d42cc8b34677467695092d
MARK OKAFOR <okaformark@gmail.com>
2024-07-23 11:35:27 -0500

    add argv parameter

--- docker-setup/pipeline.py
+++ docker-setup/pipeline.py
@@ -1,4 +1,12 @@
 import pandas as pd
+import sys
+
+print(sys.argv)
+
+day = sys.argv[1]
 df = pd.DataFrame({'a': [1, 2, 3], 'b': [4, 5, 6]})
 
 print(df)
+print(f"Day: {day}")
+print("Pipeline executed successfully")
+print(sys.argv[0])
